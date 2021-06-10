2535386335acdc56a2991b14f98c5c699d4d1689
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-10 18:25:52 +0300

    mixit

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,8 @@
-$(function () {});
+$(function () {
+  // import mixitup from 'mixitup';
+  var mixer = mixitup('.portfolio__items');
+
+  // var containerEl = document.querySelector('.portfolio__items');
+  //
+  // var mixer = mixitup(containerEl);
+});
