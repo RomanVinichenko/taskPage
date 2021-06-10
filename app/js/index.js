97ca81ed6975e0f242ca43cd04deb8d3d5e33398
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-10 18:34:58 +0300

    fill 2 pages

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,5 @@
 $(function () {
-  // import mixitup from 'mixitup';
-  var mixer = mixitup('.portfolio__items');
+  var containerEl = document.querySelector('.portfolio__items');
 
-  // var containerEl = document.querySelector('.portfolio__items');
-  //
-  // var mixer = mixitup(containerEl);
+  var mixer = mixitup(containerEl);
 });
