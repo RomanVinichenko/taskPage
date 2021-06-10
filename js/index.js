7105ade05f0098f64d3443cb5081ca1b0af39220
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-10 21:33:57 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
-$(function () {
-  var containerEl = document.querySelector('.portfolio__items');
+// $(function () {
+//   // var containerEl = document.querySelector('.portfolio__items');
+//   //
+//   // var mixer = mixitup(containerEl);
+// });
+const filterBox = document.querySelectorAll('.portfolio__item');
 
-  var mixer = mixitup(containerEl);
+document.querySelector('.portfolio__filter-items').addEventListener('click', (event) => {
+  if (event.target.tagName !== 'BUTTON') return false;
+
+  let filterClass = event.target.dataset['filter'];
+  console.log(filterClass);
+
+  filterBox.forEach((elem) => {
+    elem.classList.remove('hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      elem.classList.add('hide');
+    }
+  });
 });
